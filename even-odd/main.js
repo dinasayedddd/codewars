1944d8d543f316ce212f7a1bd5f157d78c5707a6
Dina Sayed <149122271+dinasayedddd@users.noreply.github.com>
2025-10-13 11:21:10 +0300

    Add files via upload
    even-odd

---
+++ even-odd/main.js
@@ -0,0 +1,38 @@
+//1. Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number){
+return number % 2 == 0 ? "Even" : "Odd" ;
+}
+evenOrOdd(20);
+
+
+// function evenOrOdd(number){
+// if(number%2===0) {
+//     return "Even";
+// }
+// else {
+//     return "Odd";
+// }
+// }
+// evenOrOdd(20);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
