b3cfcb9c86dfc75317838821184175e268c55e60
Dina Sayed <149122271+dinasayedddd@users.noreply.github.com>
2025-10-14 12:10:02 +0300

    Add files via upload
    String-repeat

---
+++ 4-String-repeat./main.js
@@ -0,0 +1,39 @@
+//4.  String-repeat
+
+
+//1
+function repeatStr(num,str){
+    return str.repeat(num);
+}
+console.log(repeatStr(3," hello "));
+
+
+//2
+// function repeatStr(num,str){
+//     let newString="";
+//     for(var i=0;i<num;i++){
+//         newString+=str;
+//     }
+//     return result;
+// }
+
+// console.log(repeatStr(4,"dina"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
